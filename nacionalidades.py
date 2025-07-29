21631857b7da051d91ab6b860713405e934d5efd
Jluisc3to <j.cerrada@correo.unimet.edu.ve>
2025-07-29 00:43:22 -0400

    Add files via upload

--- nacionalidades.py
+++ nacionalidades.py
@@ -1,6 +1,19 @@
 archivo=open("CH_Nationality_List_20171130_v1.csv","r")
-nacionalidades=archivo.read()
+lista_nac=list(archivo)
+lista_nac.remove('Nationality\n')
+lista_nac = [i.replace('\n','') for i in lista_nac]
 archivo.close()
-print(nacionalidades)
-# for i in nacionalidades:
-#     print(nacionalidades.index([i])+ "- " + [i])
+def nac():
+    x=0
+    for i in lista_nac:
+        print(f'{x}-{i}')
+        x+=1
+    nac_selec=int(input('Ingrese el numero de la nacionalidad escogida'))
+    if nac_selec>=225:
+        print('No es valido, ingrese un numero natural que este dentro del intervalo')
+        nac()
+    elif nac_selec>1 or nac_selec<224:
+        print(lista_nac[nac_selec])      
+        pass 
+nac()
+    
