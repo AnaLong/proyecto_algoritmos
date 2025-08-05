9705244033a9dbbd9b1f741865d9930b287c2b64
AngeloGL-tech <82678684+AngeloGL-tech@users.noreply.github.com>
2025-08-04 22:50:07 -0400

    Modificacion Obra.py
    Modificacion de la clase obra agregando atributos para adaptarla a la nueva clase Artista

--- Obra.py
+++ Obra.py
@@ -24,24 +24,21 @@ class Obra:
         self.autor=autor
 
     def show_resumen(self):
-        print(f"    Id: {self.id} - Título: {self.titulo} (Artista: {self.autor})")
+        print(f"    Id: {self.id} - Título: {self.titulo} (Artista: {self.autor.nombre})")
 
 
 class ObraDetallada:
-    def __init__(self,id,titulo,autor,nacionalidad,fecha_nacimiento,fecha_muerte,tipo,anio_creacion,imagen):
+    def __init__(self,id,titulo,autor,tipo,anio_creacion,imagen):
         super().__init__(id,titulo,autor)
-        self.nacionalidad=nacionalidad
-        self.fecha_nacimiento=fecha_nacimiento
-        self.fecha_muerte=fecha_muerte
         self.tipo=tipo
         self.anio_creacion=anio_creacion
         self.imagen=imagen
 
     def show_detalles(self):
         print(f"\nTítulo de la Obra: {self.titulo}\n"
-            f"Nombre del Artista: {self.autor} \n"
-            f"Nacionalidad del Artista: {self.nacionalidad}\n"
-            f"Fecha de nacimiento del Artista: {self.fecha_nacimiento}\n"
-            f"Fecha de muerte del Artista: {self.fecha_muerte}\n"
+            f"Nombre del Artista: {self.autor.nombre} \n"
+            f"Nacionalidad del Artista: {self.autor.nacionalidad}\n"
+            f"Fecha de nacimiento del Artista: {self.autor.nacimiento}\n"
+            f"Fecha de muerte del Artista: {self.autor.muerte}\n"
             f"Clasificación de la Obra: {self.tipo}\n"
             f"Fecha de creación de la Obra: {self.anio_creacion}\n")
