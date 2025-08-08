60beaf9b51683b75d0f8f8a761b85afb53aaf156
Ana Longobardi <ana.longobardi@correo.unimet.edu.ve>
2025-08-08 14:34:48 -0400

    Update Nacionalidad.py

--- Nacionalidad.py
+++ Nacionalidad.py
@@ -1,4 +1,15 @@
 class Nacionalidad:
+    """
+    Clase: Nacionalidad.
+
+    Objetivo: Ordenar la información suministrada por el documento de las nacionalidades disponibles.
+
+    La información requerida (atributos) es:
+    Nombre de la nacionalidad (string)
+
+    Métodos:
+    El método show muestra al usuario la lista de nacionalidades disponibles para la busqueda
+    """
     def __init__(self,nombre):
         self.nombre=nombre
     
